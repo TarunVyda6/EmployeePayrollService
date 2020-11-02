8520b680a448c86cfcc716058cb165149bf577ec
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-11-02 09:48:22 +0530

    Added : Feature to read data from file

--- EmployeePayrollService/src/main/java/EmployeePayrollService/EmployeePayrollFileIOService.java
+++ EmployeePayrollService/src/main/java/EmployeePayrollService/EmployeePayrollFileIOService.java
@@ -3,12 +3,16 @@ package EmployeePayrollService;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeePayrollFileIOService {
 
 	public static final String PAYROLL_FILE = "employee-payroll-file.txt";
 
+	/**
+	 * @param employeeList writes the employee payroll data into the file
+	 */
 	public void writeData(List<EmployeePayroll> employeeList) {
 		StringBuffer employeeBufferString = new StringBuffer();
 		employeeList.forEach(employee -> {
@@ -24,6 +28,28 @@ public class EmployeePayrollFileIOService {
 		}
 	}
 
+	/**
+	 * @return list of employees along with their details
+	 */
+	public List<EmployeePayroll> readData() {
+		List<EmployeePayroll> employeeReadList = new ArrayList<EmployeePayroll>();
+		try {
+			Files.lines(Paths.get(PAYROLL_FILE)).map(line -> line.trim()).forEach(line -> {
+				String[] data = line.split("[a-zA-Z]+ : ");
+				int id = Character.getNumericValue(data[1].charAt(0));
+				String name = data[2];
+				double salary = Double.parseDouble(data[3]);
+				EmployeePayroll employeeobject = new EmployeePayroll(id, name, salary);
+				employeeReadList.add(employeeobject);
+			});
+		} catch (IOException e) {
+		}
+		return employeeReadList;
+	}
+
+	/**
+	 * @return no of entries present in the file
+	 */
 	public long countEntries() {
 		long countOfEntries = 0;
 		try {
@@ -33,6 +59,9 @@ public class EmployeePayrollFileIOService {
 		return countOfEntries;
 	}
 
+	/**
+	 * print all the employee pay roll data in console
+	 */
 	public void printEmployeePayrolls() {
 		try {
 			Files.lines(Paths.get(PAYROLL_FILE)).forEach(System.out::println);
